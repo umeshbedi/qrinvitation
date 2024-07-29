b522afa3aee3b50dffbbad59556f93d3bd7a5268
umeshbedi <ashramumesh@gmail.com>
2024-07-29 22:23:11 +0530

    change in useffect

--- app/page.jsx
+++ app/page.jsx
@@ -61,7 +61,8 @@ export default function Home() {
 
 
   useEffect(() => {
-    return () => callSheetAction('get')
+    console.log("trying to get data..."); 
+    callSheetAction('get')
   }, [])
 
 
